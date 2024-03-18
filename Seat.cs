9eda603fcc970c137c3bddd9e67bd7335cc36e60
linkverk <125970229+linkverk@users.noreply.github.com>
2024-03-18 10:55:20 +0100

    Update Seat.cs

--- Seat.cs
+++ Seat.cs
@@ -2,4 +2,17 @@ public class Seat
 {
     public int SeatNumber { get; set; }
     public bool IsReserved { get; set; }
+
+    public void ReserveSeat()
+    {
+        if (!IsReserved)
+        {
+            IsReserved = true;
+            Console.WriteLine("Seat " + SeatNumber + " has been reserved.");
+        }
+        else
+        {
+            Console.WriteLine("Seat " + SeatNumber + " is already reserved.");
+        }
+    }
 }
