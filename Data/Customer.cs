b2168acf7c950048b357bcec663ce9772fe1cbbc
NebyuBekele <72103069+NebyuBekele@users.noreply.github.com>
2024-04-18 13:09:34 +0200

    Customer moet ook een email hebben

--- Data/Customer.cs
+++ Data/Customer.cs
@@ -5,6 +5,7 @@ namespace Cinema.Data
     public int Id { get; set; }
     public string Username { get; set; }
     public string Password { get; set; }
+    public string Email { get; set; }
 
   }
 }
