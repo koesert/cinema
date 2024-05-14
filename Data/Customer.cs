bdbc5a89831d87a6b3d4241986bcc1a07bc141c7
NebyuBekele <72103069+NebyuBekele@users.noreply.github.com>
2024-05-14 21:04:03 +0200

    method om te kijken als een email al bestaat

--- Data/Customer.cs
+++ Data/Customer.cs
@@ -38,6 +38,19 @@ namespace Cinema.Data
 			return null;
 		}
 
+		public static bool CheckEmailCustomer(CinemaContext db, string email)
+		{
+			RetrieveCustomers(db);
+
+			foreach (Customer existingCustomer in AllCustomers)
+			{
+				if (existingCustomer.Email == email)
+				{
+					return true;
+				}
+			}
+			return false;
+		}
 		public static Customer CreateCustomer(CinemaContext db, string username, string password, string email)
 		{
 			Customer newCustomer = new Customer(username, password, email);
