29254980100888fa553992d07b30e09b2b2dc745
NebyuBekele <72103069+NebyuBekele@users.noreply.github.com>
2024-05-16 14:05:53 +0200

    CancelledAt is nu null als het geen waarde heeft

--- Data/Ticket.cs
+++ Data/Ticket.cs
@@ -11,7 +11,7 @@ namespace Cinema.Data
         public Customer Customer { get; set; }
         public string CustomerEmail { get; set; }
         public DateTimeOffset PurchasedAt { get; set; }
-        public DateTimeOffset CancelledAt { get; set; }
+        public DateTimeOffset? CancelledAt { get; set; }
         public ICollection<CinemaSeat> Seats { get; set; } = new List<CinemaSeat>();
         public decimal PurchaseTotal { get; set; }
 
